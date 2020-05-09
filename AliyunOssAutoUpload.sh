3029b9704de1a5f5d61fb3bfca2e7a0e2bd858c9
Min.Ling <kid--l@hotmail.com>
2020-05-09 17:07:16 +0800

    fix methods, and remove unusage variable

--- AliyunOssAutoUpload.sh
+++ AliyunOssAutoUpload.sh
@@ -1,38 +1,41 @@
 #!/bin/bash
 
-host="your-oss-host"
-bucket="your-oss-bucket"
-Id="your-oss-id"
-Key="your-oss-key"
+# ==================== Config ====================
 
-osshost=$bucket.$host
+host="your-oss-host"
+bucket="your-oss-bucket-name"
+id="your-oss-bucket-id"
+key="your-oss-bucket-key"
+cloudFolder="your-oss-bucket-folder"
+
+# ================================================
+
 declare -a result=()
 
-for var in "$@"
-do 
-    source=$var
-    cloudDir="$(basename $var)"
-    resource="/${bucket}/${cloudDir}"
-    contentType=`file -ib ${source} |awk -F ";" '{print $1}'`
+function upload(){
+    cloudDir="$cloudFolder/$(basename $var)"
+    contentType=`file -ib $1 |awk -F ";" '{print $1}'`
     dateValue="`TZ=GMT env LANG=en_US.UTF-8 date +'%a, %d %b %Y %H:%M:%S GMT'`"
-    stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
-    signature=`echo -en $stringToSign | openssl sha1 -hmac ${Key} -binary | base64`
+    stringToSign="PUT\n\n${contentType}\n${dateValue}\n/${bucket}/${cloudDir}"
+    signature=`echo -en $stringToSign | openssl sha1 -hmac ${key} -binary | base64`
 
-    url=https://${osshost}/${cloudDir}
-
-    curl -i -q -X PUT -T "${source}" \
-        -H "Host: ${osshost}" \
+    curl -i -q -X PUT -T "$1" \
+        -H "Host: $bucket.$host" \
         -H "Date: ${dateValue}" \
         -H "Content-Type: ${contentType}" \
-        -H "Authorization: OSS ${Id}:${signature}" \
-        ${url}
+        -H "Authorization: OSS ${id}:${signature}" \
+        https://$bucket.$host/${cloudDir}
 
-    result+=(${url})
+    result+=(https://$bucket.$host/${cloudDir})
+}
+
+for var in "$@"
+do
+    upload $var
 done
 
 echo "Upload Success:"
-
 for res in "${result[@]}"
 do
-    echo "$res"
+    echo $res
 done
