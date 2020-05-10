74d985c3c0b1db4bf26c107a1bd50c8857861db2
lingme <kid--l@hotmail.com>
2020-05-10 19:51:58 +0800

    update rename method

--- AliyunOssAutoUpload.sh
+++ AliyunOssAutoUpload.sh
@@ -11,38 +11,38 @@ cloudFolder="your-oss-folder"
 # ================================================
 
 declare -a result=()
-strEncode=""
+encodeFilename=""
 
 function upload(){
-    urlencode "$(basename $1)"
-    cloudDir="$cloudFolder/$strEncode"
-    contentType=`file -ib $1 |awk -F ";" '{print $1}'`
+    urlencode "$(basename "$1")"
+    cloudDir="$cloudFolder/$encodeFilename"
+    contentType=`file -b --mime-type "$1"`
     dateValue="`TZ=GMT env LANG=en_US.UTF-8 date +'%a, %d %b %Y %H:%M:%S GMT'`"
-    stringToSign="PUT\n\n${contentType}\n${dateValue}\n/${bucket}/${cloudFolder}/$(basename $1)"
-    signature=`echo -en $stringToSign | openssl sha1 -hmac ${key} -binary | base64`
+    stringToSign="PUT\n\n$contentType\n$dateValue\n/$bucket/$cloudFolder/$encodeFilename"
+    signature=`echo -en "$stringToSign" | openssl sha1 -hmac "$key" -binary | base64`
 
     curl -i -q -X PUT -T "$1" \
         -H "Host: $bucket.$host" \
-        -H "Date: ${dateValue}" \
-        -H "Content-Type: ${contentType}" \
-        -H "Authorization: OSS ${id}:${signature}" \
-        https://$bucket.$host/${cloudDir}
+        -H "Date: $dateValue" \
+        -H "Content-Type: $contentType" \
+        -H "Authorization: OSS $id:$signature" \
+        https://"$bucket"."$host"/"$cloudDir"
 
-    result+=(https://$bucket.$host/${cloudDir})
+    result+=(https://"$bucket"."$host"/"$cloudDir")
 }
 
 function urlencode() {
-  strEncode=""
+  encodeFilename=""
   local length="${#1}"
   for (( i = 0; i < length; i++ ))
   do
     local c="${1:i:1}"
     case $c in 
       [a-zA-Z0-9.~_-]) 
-        strEncode=$strEncode$(printf "$c") 
+        encodeFilename=$encodeFilename$(printf "$c") 
         ;;
       *) 
-        strEncode=$strEncode$(printf "$c" | xxd -p -c1 | 
+        encodeFilename=$encodeFilename$(printf "$c" | xxd -p -c1 | 
         while read x
         do 
           printf "%%%s" "$x"
@@ -53,11 +53,12 @@ function urlencode() {
 
 for var in "$@"
 do
-    upload $var
+    newFile="${var%/*}/"`echo "${var##*/}" | sed 's/[ ][ ]*/_/g'`
+    mv "$var" "$newFile" 
+    upload "$newFile"
 done
 
-echo "Upload Success:"
 for res in "${result[@]}"
 do
-    echo $res
+    echo "$res"
 done
